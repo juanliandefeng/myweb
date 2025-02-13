04a54583516ecc598cd081fa0f5bd848c7f11afc
juanliandefeng <1140439515@qq.com>
2025-02-13 16:29:16 +0800

    Update productlist.js

--- js/productlist.js
+++ js/productlist.js
@@ -4,7 +4,7 @@ const app = new Vue({
         products: [],
         listMessage: '',
         currentPage: 1,
-        perPage: 2,
+        perPage: 10,
         pageList: '',
         pageNum: '',
         url: 'detail.html?id=',
@@ -15,10 +15,10 @@ const app = new Vue({
             var URLdata = new URLSearchParams(window.location.search)
             var URLdata1 = URLdata.get('key')
             this.productName = URLdata1
-            console.log(URLdata1)
+            //console.log(URLdata1)
             if(URLdata1){
                 var URLdata2 = URLdata1.split(' ')
-                console.log(URLdata2[0] + '1')
+                //console.log(URLdata2[0] + '1')
                 this.products = tongdata.filter(item => {
                     console.log(item)
                 return item.type.toLowerCase().includes(URLdata2[0].toLowerCase()) &&
@@ -33,7 +33,7 @@ const app = new Vue({
         },
         //creat
         creatPage(){
-            console.log(this.listMessage)
+            //console.log(this.listMessage)
             this.pageNum =  Math.ceil(this.listMessage.length / this.perPage)
             //console.log(this.pageNum)
             
@@ -43,28 +43,14 @@ const app = new Vue({
         },
         searchPage(index){
             this.currentPage = index
-            console.log(index)
+            //console.log(index)
             //window.location.href = '分页器.html?page=' + index
             this.creatPage()
         },
-        // refreshWithParam(lis) {
-        //     // 创建新 URL 对象
-        //     const url = new URL(window.location.href);
-        //     // 设置/更新参数
-        //     url.searchParams.set('page', lis);
-        //     // 跳转到新 URL 实现刷新
-        //     window.location.href = url.toString();
-        // }
     },
     mounted (){
         this.setMessage()
         this.creatPage()
-        // window.addEventListener('load',() => {
-        //         const params = new URLSearchParams(window.location.search)
-        //         var theme = params.get('page')
-        //         //console.log(theme)
-        //         this.currentPage = theme
-        //         alert(this.currentPage)
-        //     })
+        
     }
 })
