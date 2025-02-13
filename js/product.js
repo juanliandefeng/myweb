ca55f7ab3391df4db365a281bf97c320e0b15c59
juanliandefeng <1140439515@qq.com>
2025-02-13 16:29:40 +0800

    Update product.js

--- js/product.js
+++ js/product.js
@@ -1,6 +1,6 @@
 var tongdata = [
     {   
-        id:1,
+        id: 1,
         name:'Life Size Bronze Golfer Statue',
         num: 'FGTS-001',
         Material: 'bronze',
@@ -24,12 +24,12 @@ var tongdata = [
             'img/bronze/golf-player4.jpg',
         ]
     },
-    {
+    {   
         id:2,
-        name:'Life Size Bronze Golfer Statue',
-        num: 'FGTS-002',
-        Material: 'bronze',
-        type: 'bronze Golfer other sculpture',
+        name:'Bronze Cherub Angel Garden Statues',
+        num: 'XTSD-001',
+        Material: 'Bronze/ Brass / Copper',
+        type: 'bronze angel garden sculpture',
         price: 'USD 800-3900 per piece',
         Payment: 'T/T, PayPal, Western Union or other ways',
         delivery: 'period: 30-45days',
@@ -37,21 +37,24 @@ var tongdata = [
         PackageOut: 'strong wooden cases or iron frame boxes',
         Categories: 'Bronze Sculpture, Sport Statue',
         introduction: [
-            'This is a bronze golfer statue. The sculpture fixes the moment when a golfer swings his golf club, making time seem to come to a standstill at that moment. Every line and angle is carefully designed, and the player’s body twists to form a tense posture, reflecting the perfect combination of strength and elegance.',
+            'This is a bronze angel statue. The sculpture fixes the moment when a golfer swings his golf club, making time seem to come to a standstill at that moment. Every line and angle is carefully designed, and the player’s body twists to form a tense posture, reflecting the perfect combination of strength and elegance.',
             'Bronze statues of golfers are widely used to decorate and beautify the environment in golf clubs, tournament venues, and other places. They can be placed at the entrance of the stadium, club hall, or game memorial area, becoming a beautiful scenery.',
             'Bronze golf player statues are increasingly appearing in public spaces in cities, such as parks, squares, and even commercial centers. They have become a part of the urban landscape, enriching the cultural life of citizens and providing tourists with more opportunities to learn about local characteristics.',
             'We are committed to providing you with unique sculpture customization services. Whether you are seeking an artwork that showcases your personal taste or hoping to add iconic landscapes to your business or public space, our professional team can carefully design and produce sculptures that meet your specific needs and vision.'
         ],
         img: [
-            'img/bronze/golf-player1.jpg'
+            'img/bronze/angel-garden.jpg',
+            'img/bronze/angel-garden1.jpg',
+            'img/bronze/angel-garden2.jpg',
+            'img/bronze/angel-garden3.jpg',
         ]
     },
-    {
-        id:3,
-        name:'Life Size Bronze Golfer Statue',
-        num: 'FGTS-003',
-        Material: 'bronze',
-        type: 'bronze angel sculpture',
+    {   
+        id: 3,
+        name:'Bronze Life Size Bear Sculpture Fighting With Eagle',
+        num: 'QIXY-001',
+        Material: 'Bronze/ Brass / Copper',
+        type: 'bronze bear Eagle sculpture',
         price: 'USD 800-3900 per piece',
         Payment: 'T/T, PayPal, Western Union or other ways',
         delivery: 'period: 30-45days',
@@ -59,13 +62,166 @@ var tongdata = [
         PackageOut: 'strong wooden cases or iron frame boxes',
         Categories: 'Bronze Sculpture, Sport Statue',
         introduction: [
-            'This is a bronze golfer statue. The sculpture fixes the moment when a golfer swings his golf club, making time seem to come to a standstill at that moment. Every line and angle is carefully designed, and the player’s body twists to form a tense posture, reflecting the perfect combination of strength and elegance.',
+            'This is a bronze bear statue. The sculpture fixes the moment when a golfer swings his golf club, making time seem to come to a standstill at that moment. Every line and angle is carefully designed, and the player’s body twists to form a tense posture, reflecting the perfect combination of strength and elegance.',
             'Bronze statues of golfers are widely used to decorate and beautify the environment in golf clubs, tournament venues, and other places. They can be placed at the entrance of the stadium, club hall, or game memorial area, becoming a beautiful scenery.',
             'Bronze golf player statues are increasingly appearing in public spaces in cities, such as parks, squares, and even commercial centers. They have become a part of the urban landscape, enriching the cultural life of citizens and providing tourists with more opportunities to learn about local characteristics.',
             'We are committed to providing you with unique sculpture customization services. Whether you are seeking an artwork that showcases your personal taste or hoping to add iconic landscapes to your business or public space, our professional team can carefully design and produce sculptures that meet your specific needs and vision.'
         ],
         img: [
-            'img/bronze/golf-player1.jpg'
+            'img/bronze/bear1.jpg',
+            'img/bronze/bear2.jpg',
+            'img/bronze/bear3.jpg',
+            'img/bronze/bear4.jpg',
+        ]
+    },
+    {   
+        id: 4,
+        name:'Bronze ballet dancer figurines',
+        num: 'QIXY-201',
+        Material: 'Bronze/ Brass / Copper',
+        type: 'bronze ballet dancer sculpture',
+        price: 'USD 800-3900 per piece',
+        Payment: 'T/T, PayPal, Western Union or other ways',
+        delivery: 'period: 30-45days',
+        PackageIn: 'soft plastic foam/ blanket',
+        PackageOut: 'strong wooden cases or iron frame boxes',
+        Categories: 'Bronze Sculpture, Sport Statue',
+        introduction: [
+            'This is a bronze bear statue. The sculpture fixes the moment when a golfer swings his golf club, making time seem to come to a standstill at that moment. Every line and angle is carefully designed, and the player’s body twists to form a tense posture, reflecting the perfect combination of strength and elegance.',
+            'Bronze statues of golfers are widely used to decorate and beautify the environment in golf clubs, tournament venues, and other places. They can be placed at the entrance of the stadium, club hall, or game memorial area, becoming a beautiful scenery.',
+            'Bronze golf player statues are increasingly appearing in public spaces in cities, such as parks, squares, and even commercial centers. They have become a part of the urban landscape, enriching the cultural life of citizens and providing tourists with more opportunities to learn about local characteristics.',
+            'We are committed to providing you with unique sculpture customization services. Whether you are seeking an artwork that showcases your personal taste or hoping to add iconic landscapes to your business or public space, our professional team can carefully design and produce sculptures that meet your specific needs and vision.'
+        ],
+        img: [
+            'img/bronze/dancer1.jpg',
+            'img/bronze/dancer2.jpg',
+            'img/bronze/dancer3.jpg',
+            'img/bronze/dancer4.jpg',
+        ]
+    },
+    {   
+        id: 5,
+        name:'Bronze Girl Reading Sculpture Garden Statue',
+        num: 'QIXY-261',
+        Material: 'Bronze/ Brass / Copper',
+        type: 'bronze Girl Reading sculpture',
+        price: 'USD 800-3900 per piece',
+        Payment: 'T/T, PayPal, Western Union or other ways',
+        delivery: 'period: 30-45days',
+        PackageIn: 'soft plastic foam/ blanket',
+        PackageOut: 'strong wooden cases or iron frame boxes',
+        Categories: 'Bronze Sculpture, Sport Statue',
+        introduction: [
+            'This is a bronze bear statue. The sculpture fixes the moment when a golfer swings his golf club, making time seem to come to a standstill at that moment. Every line and angle is carefully designed, and the player’s body twists to form a tense posture, reflecting the perfect combination of strength and elegance.',
+            'Bronze statues of golfers are widely used to decorate and beautify the environment in golf clubs, tournament venues, and other places. They can be placed at the entrance of the stadium, club hall, or game memorial area, becoming a beautiful scenery.',
+            'Bronze golf player statues are increasingly appearing in public spaces in cities, such as parks, squares, and even commercial centers. They have become a part of the urban landscape, enriching the cultural life of citizens and providing tourists with more opportunities to learn about local characteristics.',
+            'We are committed to providing you with unique sculpture customization services. Whether you are seeking an artwork that showcases your personal taste or hoping to add iconic landscapes to your business or public space, our professional team can carefully design and produce sculptures that meet your specific needs and vision.'
+        ],
+        img: [
+            'img/bronze/girlonglobe1.jpg',
+            'img/bronze/girlonglobe2.jpg',
+            'img/bronze/girlonglobe3.jpg',
+            'img/bronze/girlonglobe4.jpg',
+        ]
+    },
+    {   
+        id: 6,
+        name:'Animal Garden Sculpture Bronze Doe and Fawn Statue',
+        num: 'QIXY-041',
+        Material: 'Bronze/ Brass / Copper',
+        type: 'bronze Animal Doe sculpture',
+        price: 'USD 800-3900 per piece',
+        Payment: 'T/T, PayPal, Western Union or other ways',
+        delivery: 'period: 30-45days',
+        PackageIn: 'soft plastic foam/ blanket',
+        PackageOut: 'strong wooden cases or iron frame boxes',
+        Categories: 'Bronze Sculpture, Sport Statue',
+        introduction: [
+            'This is a bronze bear statue. The sculpture fixes the moment when a golfer swings his golf club, making time seem to come to a standstill at that moment. Every line and angle is carefully designed, and the player’s body twists to form a tense posture, reflecting the perfect combination of strength and elegance.',
+            'Bronze statues of golfers are widely used to decorate and beautify the environment in golf clubs, tournament venues, and other places. They can be placed at the entrance of the stadium, club hall, or game memorial area, becoming a beautiful scenery.',
+            'Bronze golf player statues are increasingly appearing in public spaces in cities, such as parks, squares, and even commercial centers. They have become a part of the urban landscape, enriching the cultural life of citizens and providing tourists with more opportunities to learn about local characteristics.',
+            'We are committed to providing you with unique sculpture customization services. Whether you are seeking an artwork that showcases your personal taste or hoping to add iconic landscapes to your business or public space, our professional team can carefully design and produce sculptures that meet your specific needs and vision.'
+        ],
+        img: [
+            'img/bronze/Doe1.jpg',
+            'img/bronze/Doe2.jpg',
+            'img/bronze/Doe3.jpg',
+            'img/bronze/Doe4.jpg',
+        ]
+    },
+    {   
+        id: 7,
+        name:'Life Size Bronze Paired Elephant Statue at Entrance',
+        num: 'QIXY-022',
+        Material: 'Bronze/ Brass / Copper',
+        type: 'bronze Animal Elephant sculpture',
+        price: 'USD 800-3900 per piece',
+        Payment: 'T/T, PayPal, Western Union or other ways',
+        delivery: 'period: 30-45days',
+        PackageIn: 'soft plastic foam/ blanket',
+        PackageOut: 'strong wooden cases or iron frame boxes',
+        Categories: 'Bronze Sculpture, Sport Statue',
+        introduction: [
+            'This is a bronze bear statue. The sculpture fixes the moment when a golfer swings his golf club, making time seem to come to a standstill at that moment. Every line and angle is carefully designed, and the player’s body twists to form a tense posture, reflecting the perfect combination of strength and elegance.',
+            'Bronze statues of golfers are widely used to decorate and beautify the environment in golf clubs, tournament venues, and other places. They can be placed at the entrance of the stadium, club hall, or game memorial area, becoming a beautiful scenery.',
+            'Bronze golf player statues are increasingly appearing in public spaces in cities, such as parks, squares, and even commercial centers. They have become a part of the urban landscape, enriching the cultural life of citizens and providing tourists with more opportunities to learn about local characteristics.',
+            'We are committed to providing you with unique sculpture customization services. Whether you are seeking an artwork that showcases your personal taste or hoping to add iconic landscapes to your business or public space, our professional team can carefully design and produce sculptures that meet your specific needs and vision.'
+        ],
+        img: [
+            'img/bronze/Elephant-1.jpg',
+            'img/bronze/Elephant-2.jpg',
+            'img/bronze/Elephant-3.jpg',
+            'img/bronze/Elephant-4.jpg',
+        ]
+    },
+    {   
+        id: 8,
+        name:'Outdoor Bronze Woman Water Fountain',
+        num: 'QIXY-022',
+        Material: 'Bronze/ Brass / Copper',
+        type: 'bronze Woman Fountain sculpture',
+        price: 'USD 800-3900 per piece',
+        Payment: 'T/T, PayPal, Western Union or other ways',
+        delivery: 'period: 30-45days',
+        PackageIn: 'soft plastic foam/ blanket',
+        PackageOut: 'strong wooden cases or iron frame boxes',
+        Categories: 'Bronze Sculpture, Sport Statue',
+        introduction: [
+            'This is a bronze bear statue. The sculpture fixes the moment when a golfer swings his golf club, making time seem to come to a standstill at that moment. Every line and angle is carefully designed, and the player’s body twists to form a tense posture, reflecting the perfect combination of strength and elegance.',
+            'Bronze statues of golfers are widely used to decorate and beautify the environment in golf clubs, tournament venues, and other places. They can be placed at the entrance of the stadium, club hall, or game memorial area, becoming a beautiful scenery.',
+            'Bronze golf player statues are increasingly appearing in public spaces in cities, such as parks, squares, and even commercial centers. They have become a part of the urban landscape, enriching the cultural life of citizens and providing tourists with more opportunities to learn about local characteristics.',
+            'We are committed to providing you with unique sculpture customization services. Whether you are seeking an artwork that showcases your personal taste or hoping to add iconic landscapes to your business or public space, our professional team can carefully design and produce sculptures that meet your specific needs and vision.'
+        ],
+        img: [
+            'img/bronze/woman-1.jpg',
+            'img/bronze/woman-2.jpg',
+            'img/bronze/woman-3.jpg',
+            'img/bronze/woman-4.jpg',
+        ]
+    },
+    {   
+        id: 9,
+        name:'Life size bronze horse sculpture',
+        num: 'FGTS-022',
+        Material: 'Bronze/ Brass / Copper',
+        type: 'bronze horse sculpture',
+        price: 'USD 800-3900 per piece',
+        Payment: 'T/T, PayPal, Western Union or other ways',
+        delivery: 'period: 30-45days',
+        PackageIn: 'soft plastic foam/ blanket',
+        PackageOut: 'strong wooden cases or iron frame boxes',
+        Categories: 'Bronze Sculpture, Sport Statue',
+        introduction: [
+            'This is a bronze horse statue. The sculpture fixes the moment when a golfer swings his golf club, making time seem to come to a standstill at that moment. Every line and angle is carefully designed, and the player’s body twists to form a tense posture, reflecting the perfect combination of strength and elegance.',
+            'Bronze statues of golfers are widely used to decorate and beautify the environment in golf clubs, tournament venues, and other places. They can be placed at the entrance of the stadium, club hall, or game memorial area, becoming a beautiful scenery.',
+            'Bronze golf player statues are increasingly appearing in public spaces in cities, such as parks, squares, and even commercial centers. They have become a part of the urban landscape, enriching the cultural life of citizens and providing tourists with more opportunities to learn about local characteristics.',
+            'We are committed to providing you with unique sculpture customization services. Whether you are seeking an artwork that showcases your personal taste or hoping to add iconic landscapes to your business or public space, our professional team can carefully design and produce sculptures that meet your specific needs and vision.'
+        ],
+        img: [
+            'img/bronze/horse-1.jpg',
+            'img/bronze/horse-2.jpg',
+            'img/bronze/horse-3.jpg',
+            'img/bronze/horse-4.jpg',
         ]
     },
 ]
