a1b4da48e9e9811fc2c6bfd97d562b0b5aca452e
juanliandefeng <1140439515@qq.com>
2025-02-17 08:25:31 +0800

    Update product.js

--- js/product.js
+++ js/product.js
@@ -224,4 +224,54 @@ var tongdata = [
             'img/bronze/horse-4.jpg',
         ]
     },
+    {   
+        id: 10,
+        name:'Abstract bronze sculpture for sale',
+        num: 'CXDS-001',
+        Material: 'Bronze/ Brass / Copper',
+        type: 'bronze Abstract other sculpture',
+        price: 'USD 800-3900 per piece',
+        Payment: 'T/T, PayPal, Western Union or other ways',
+        delivery: 'period: 30-45days',
+        PackageIn: 'soft plastic foam/ blanket',
+        PackageOut: 'strong wooden cases or iron frame boxes',
+        Categories: 'Bronze Sculpture, Sport Statue',
+        introduction: [
+            'This is a bronze abstract statue. The sculpture fixes the moment when a golfer swings his golf club, making time seem to come to a standstill at that moment. Every line and angle is carefully designed, and the player’s body twists to form a tense posture, reflecting the perfect combination of strength and elegance.',
+            'Bronze statues of golfers are widely used to decorate and beautify the environment in golf clubs, tournament venues, and other places. They can be placed at the entrance of the stadium, club hall, or game memorial area, becoming a beautiful scenery.',
+            'Bronze golf player statues are increasingly appearing in public spaces in cities, such as parks, squares, and even commercial centers. They have become a part of the urban landscape, enriching the cultural life of citizens and providing tourists with more opportunities to learn about local characteristics.',
+            'We are committed to providing you with unique sculpture customization services. Whether you are seeking an artwork that showcases your personal taste or hoping to add iconic landscapes to your business or public space, our professional team can carefully design and produce sculptures that meet your specific needs and vision.'
+        ],
+        img: [
+            'img/bronze/abstract-1.jpg',
+            'img/bronze/abstract-2.jpg',
+            'img/bronze/abstract-3.jpg',
+            'img/bronze/abstract-4.jpg',
+        ]
+    },
+    {   
+        id: 11,
+        name:'Fiberglass Large seated kuan yin statue',
+        num: 'CXDS-001',
+        Material: 'Fiberglass',
+        type: 'Fiberglass kuan yin statue sculpture',
+        price: 'USD 800-3900 per piece',
+        Payment: 'T/T, PayPal, Western Union or other ways',
+        delivery: 'period: 30-45days',
+        PackageIn: 'soft plastic foam/ blanket',
+        PackageOut: 'strong wooden cases or iron frame boxes',
+        Categories: 'Bronze Sculpture, Sport Statue',
+        introduction: [
+            'This is a Fiberglass kuan yin statue. The sculpture fixes the moment when a golfer swings his golf club, making time seem to come to a standstill at that moment. Every line and angle is carefully designed, and the player’s body twists to form a tense posture, reflecting the perfect combination of strength and elegance.',
+            'Bronze statues of golfers are widely used to decorate and beautify the environment in golf clubs, tournament venues, and other places. They can be placed at the entrance of the stadium, club hall, or game memorial area, becoming a beautiful scenery.',
+            'Bronze golf player statues are increasingly appearing in public spaces in cities, such as parks, squares, and even commercial centers. They have become a part of the urban landscape, enriching the cultural life of citizens and providing tourists with more opportunities to learn about local characteristics.',
+            'We are committed to providing you with unique sculpture customization services. Whether you are seeking an artwork that showcases your personal taste or hoping to add iconic landscapes to your business or public space, our professional team can carefully design and produce sculptures that meet your specific needs and vision.'
+        ],
+        img: [
+            'img/bronze/guanyin-1.jpg',
+            'img/bronze/guanyin-2.jpg',
+            'img/bronze/guanyin-3.jpg',
+            'img/bronze/guanyin-4.jpg',
+        ]
+    },
 ]
