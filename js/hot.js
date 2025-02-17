1999b756d5f396a4cc8ddcb0b19093fa97f93e3a
juanliandefeng <1140439515@qq.com>
2025-02-17 08:34:16 +0800

    Update hot.js

--- js/hot.js
+++ js/hot.js
@@ -1,61 +1,14 @@
 const hot = new Vue({
     el: '#hot',
     data: {
-        hotList: [
-            {
-                id: 1,
-                url: 'img/home/robot.jpg',
-                title: 'Popular Modern Mirror Polished Stainless Steel Sculpture for Outdoor Garden Decor CSS-14',
-                text: "This product is one of our company's hottest selling and most popular abstract stainless steel sculptures. Please don't hesitate to make your choice and contact us today for detailed product information and the best quote right now. As well as we have a variety of sizes to choose from. Item No: CSS-14 Inspiration: Growing The Size: Height: 79in (200cm) or customized size The Weight: 55-65 KG Surface: Shiny Colorful Polished Service 1: Trade Assurance Service 2: Free Customized Service including size and finish colors Service 3: Factory Direct Price; Door to Door Delivery Installation Method: Freestanding with base or bolted connection. "
-            },
-            {
-                id: 2,
-                url: 'img/home/bear.jpg',
-                title: 'Bronze Rearing Horse Statue for Sale BOKK-236',
-                text: "This product is one of our company's hottest selling and most popular abstract stainless steel sculptures. Please don't hesitate to make your choice and contact us today for detailed product information and the best quote right now. As well as we have a variety of sizes to choose from. Item No: CSS-14 Inspiration: Growing The Size: Height: 79in (200cm) or customized size The Weight: 55-65 KG Surface: Shiny Colorful Polished Service 1: Trade Assurance Service 2: Free Customized Service including size and finish colors Service 3: Factory Direct Price; Door to Door Delivery Installation Method: Freestanding with base or bolted connection. "
-            },
-            {
-                id: 3,
-                url: 'img/home/bear.jpg',
-                title: 'Bronze Rearing Horse Statue for Sale BOKK-236',
-                text: "This product is one of our company's hottest selling and most popular abstract stainless steel sculptures. Please don't hesitate to make your choice and contact us today for detailed product information and the best quote right now. As well as we have a variety of sizes to choose from. Item No: CSS-14 Inspiration: Growing The Size: Height: 79in (200cm) or customized size The Weight: 55-65 KG Surface: Shiny Colorful Polished Service 1: Trade Assurance Service 2: Free Customized Service including size and finish colors Service 3: Factory Direct Price; Door to Door Delivery Installation Method: Freestanding with base or bolted connection. "
-            },
-            {
-                id: 4,
-                url: 'img/home/bear.jpg',
-                title: 'Bronze Rearing Horse Statue for Sale BOKK-236',
-                text: "This product is one of our company's hottest selling and most popular abstract stainless steel sculptures. Please don't hesitate to make your choice and contact us today for detailed product information and the best quote right now. As well as we have a variety of sizes to choose from. Item No: CSS-14 Inspiration: Growing The Size: Height: 79in (200cm) or customized size The Weight: 55-65 KG Surface: Shiny Colorful Polished Service 1: Trade Assurance Service 2: Free Customized Service including size and finish colors Service 3: Factory Direct Price; Door to Door Delivery Installation Method: Freestanding with base or bolted connection. "
-            },
-            {
-                id: 5,
-                url: 'img/home/bear.jpg',
-                title: 'Bronze Rearing Horse Statue for Sale BOKK-236',
-                text: "This product is one of our company's hottest selling and most popular abstract stainless steel sculptures. Please don't hesitate to make your choice and contact us today for detailed product information and the best quote right now. As well as we have a variety of sizes to choose from. Item No: CSS-14 Inspiration: Growing The Size: Height: 79in (200cm) or customized size The Weight: 55-65 KG Surface: Shiny Colorful Polished Service 1: Trade Assurance Service 2: Free Customized Service including size and finish colors Service 3: Factory Direct Price; Door to Door Delivery Installation Method: Freestanding with base or bolted connection. "
-            },
-            {
-                id: 6,
-                url: 'img/home/bear.jpg',
-                title: 'Bronze Rearing Horse Statue for Sale BOKK-236',
-                text: "This product is one of our company's hottest selling and most popular abstract stainless steel sculptures. Please don't hesitate to make your choice and contact us today for detailed product information and the best quote right now. As well as we have a variety of sizes to choose from. Item No: CSS-14 Inspiration: Growing The Size: Height: 79in (200cm) or customized size The Weight: 55-65 KG Surface: Shiny Colorful Polished Service 1: Trade Assurance Service 2: Free Customized Service including size and finish colors Service 3: Factory Direct Price; Door to Door Delivery Installation Method: Freestanding with base or bolted connection. "
-            },
-            {
-                id: 7,
-                url: 'img/home/bear.jpg',
-                title: 'Bronze Rearing Horse Statue for Sale BOKK-236',
-                text: "This product is one of our company's hottest selling and most popular abstract stainless steel sculptures. Please don't hesitate to make your choice and contact us today for detailed product information and the best quote right now. As well as we have a variety of sizes to choose from. Item No: CSS-14 Inspiration: Growing The Size: Height: 79in (200cm) or customized size The Weight: 55-65 KG Surface: Shiny Colorful Polished Service 1: Trade Assurance Service 2: Free Customized Service including size and finish colors Service 3: Factory Direct Price; Door to Door Delivery Installation Method: Freestanding with base or bolted connection. "
-            },
-            {
-                id: 8,
-                url: 'img/home/bear.jpg',
-                title: 'Bronze Rearing Horse Statue for Sale BOKK-236',
-                text: "This product is one of our company's hottest selling and most popular abstract stainless steel sculptures. Please don't hesitate to make your choice and contact us today for detailed product information and the best quote right now. As well as we have a variety of sizes to choose from. Item No: CSS-14 Inspiration: Growing The Size: Height: 79in (200cm) or customized size The Weight: 55-65 KG Surface: Shiny Colorful Polished Service 1: Trade Assurance Service 2: Free Customized Service including size and finish colors Service 3: Factory Direct Price; Door to Door Delivery Installation Method: Freestanding with base or bolted connection. "
-            },
-            {
-                id: 9,
-                url: 'img/home/bear.jpg',
-                title: 'Bronze Rearing Horse Statue for Sale BOKK-236',
-                text: "This product is one of our company's hottest selling and most popular abstract stainless steel sculptures. Please don't hesitate to make your choice and contact us today for detailed product information and the best quote right now. As well as we have a variety of sizes to choose from. Item No: CSS-14 Inspiration: Growing The Size: Height: 79in (200cm) or customized size The Weight: 55-65 KG Surface: Shiny Colorful Polished Service 1: Trade Assurance Service 2: Free Customized Service including size and finish colors Service 3: Factory Direct Price; Door to Door Delivery Installation Method: Freestanding with base or bolted connection. "
-            },
-        ]
+        hotList: '',
     },
+    methods: {
+        setMessage (){
+            this.hotList = tongdata.slice( -9 ).reverse()
+        },
+    },
+    mounted (){
+        this.setMessage()
+    }
 })
