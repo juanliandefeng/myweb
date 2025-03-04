b65cb4cc3bbf81a26ad7dcf9f27091260060f184
juanliandefeng <1140439515@qq.com>
2025-03-04 10:10:18 +0800

    Update productLeftNav.js

--- js/productLeftNav.js
+++ js/productLeftNav.js
@@ -8,14 +8,13 @@ var leftNav = new Vue({
     el: '#middle_top',
     data: {
         title: 'PRODUCT',
-        msg: tongdata
     },
     methods: {
         fn (){
             var URLdata = new URLSearchParams(window.location.search)
             const id = URLdata.get('id')
             if (id){
-                this.title = this.msg.find( item => item.id == id).title.toUpperCase()
+                this.title = tongdata.find( item => item.id == id).title.toUpperCase()
             }
         }
     },
